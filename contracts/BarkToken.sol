69ad9b0dc7c8df95d2a5418b8e1c4ebd5a730072
StreamPay <streamprotocol@protonmail.com>
2023-05-19 13:17:12 +0300

    first commit

---
+++ contracts/BarkToken.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+/// @custom:security-contact contact.bark@proton.me
+contract BarkToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable, UUPSUpgradeable {
+    bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    address public constant BURN_ADDRESS = 0xdead;
+
+    uint256 private constant SECONDS_IN_MONTH = 30 days;
+    uint256 private lastBurnTime;
+    uint256 private totalBurnedTokens;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("Bark Token", "BRK");
+        __ERC20Burnable_init();
+        __ERC20Snapshot_init();
+        __AccessControl_init();
+        __Pausable_init();
+        __ERC20Permit_init("Bark Token");
+        __ERC20Votes_init();
+        __ERC20FlashMint_init();
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(SNAPSHOT_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _mint(msg.sender, 5000000000 * 10 ** decimals());
+        _grantRole(UPGRADER_ROLE, msg.sender);
+
+        lastBurnTime = block.timestamp;
+    }
+
+    function snapshot() public onlyRole(SNAPSHOT_ROLE) {
+        _snapshot();
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+        _burnTokens(amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._mint(to, amount);
+        _burnTokens(amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._burn(account, amount);
+        _burnTokens(amount);
+    }
+
+    function _burnTokens(uint256 amount) private {
+        if (block.timestamp >= lastBurnTime + SECONDS_IN_MONTH) {
+            uint256 burnAmount = (totalSupply() * amount) / 1000;
+            super._burn(BURN_ADDRESS, burnAmount);
+            totalBurnedTokens += burnAmount;
+            lastBurnTime = block.timestamp;
+        }
+    }
+}
